3fc22f0a0e9cc80c3d4b2e86484a9e12cd91d521
311 <ismoilov92@gmail.com>
2022-05-26 22:44:40 +0300

    lazyLoader

--- src/main/resources/static/js/pages/MessageList.vue
+++ src/main/resources/static/js/pages/MessageList.vue
@@ -6,6 +6,9 @@
                    :key="message.id"
                    :message="message"
                    :editMessage="editMessage" />
+      <lazy-loader>
+
+      </lazy-loader>
     </v-layout>
   </v-container>
 </template>
@@ -14,9 +17,11 @@
 import { mapGetters } from 'vuex'
 import MessageRow from 'components/messages/MessageRow.vue'
 import MessageForm from 'components/messages/MessageForm.vue'
+import LazyLoader from "../components/LazyLoader.vue";
 
 export default {
   components: {
+    LazyLoader,
     MessageRow,
     MessageForm
   },
