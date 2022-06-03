e0dc2c2cc0a8a3c35fbc1044795e24f7c3eb8f17
311 <ismoilov92@gmail.com>
2022-06-03 23:05:55 +0300

    approve subscribers

--- src/main/resources/static/js/API/profile.js
+++ src/main/resources/static/js/API/profile.js
@@ -4,5 +4,7 @@ const profile=Vue.resource('/profile{/id}')
 
 export default {
     get: id=> profile.get({id}),
-    changeSubscription: channelID => Vue.http.post(`/profile/change-subscription/${channelID}`)
+    changeSubscription: channelID => Vue.http.post(`/profile/change-subscription/${channelID}`),
+    subscriberList:channelId => Vue.http.get(`/profile/get-subscribers/${channelId}`),
+    changeSubscriptionStatus: subscriberId => Vue.http.post(`/profile/change-status/${subscriberId}`)
 }
