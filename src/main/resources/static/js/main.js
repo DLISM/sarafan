d18d7d5a9733e2a0a68125aa3fd51fff66cb64e6
311 <ismoilov92@gmail.com>
2022-06-04 23:32:32 +0300

    add Sentry monitoring for frontend

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,6 +7,29 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+    Vue,
+    dsn: "https://142877bb0fc34e8b9e87f34e98b916c7@o1275240.ingest.sentry.io/6470203",
+    integrations: [
+        new BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+        }),
+    ],
+
+    tracesSampleRate: 1.0,
+});
+
+Sentry.configureScope(scope => scope.setUser({
+    id:profile && profile.id,
+    username: profile && profile.name
+
+    })
+
+)
 
 if (profile) {
     connect()
